5d59e08aa40eb9fd7b4b1fb68d71c671a9ea2290
Najla-Nuricia <najlalaudy@gmail.com>
2025-06-05 00:00:46 -0400

    Hellow_world_Linux

---
+++ src/nama.java
@@ -0,0 +1,5 @@
+public class nama {
+    public static void main(String[] args) {
+        System.out.println("Hellow world from linux debian 12");
+    }
+}
--- src/new/testing.java
+++
@@ -1,5 +0,0 @@
-package new;
-
-public class testing {
-    
-}
